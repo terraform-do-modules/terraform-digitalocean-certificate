130b795f0d72c2cff983b06db239106d6a9d2ec8
mahesh yadav <devopsmahesh.it@gmail.com>
2023-07-28 17:01:08 +0530

    fix: update source version and readme.yml file and dependabot.yml file

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@
 #Description : Provides a DigitalOcean Certificate resource that allows you to manage certificates.
 ##----------------------------------------------------------------------------------------------------
 resource "digitalocean_certificate" "custom" {
-  count = var.enabled && var.custom_certificate == true ? 1 : 0
-
+  count             = var.enabled && var.custom_certificate == true ? 1 : 0
   name              = var.certificate_name
   type              = var.type
   private_key       = var.private_key != "" ? file(var.private_key) : null
@@ -15,8 +14,7 @@ resource "digitalocean_certificate" "custom" {
 #Description :Provides a DigitalOcean Certificate resource that allows you to manage certificates.
 ##---------------------------------------------------------------------------------------------------
 resource "digitalocean_certificate" "lets_encrypt" {
-  count = var.enabled && var.custom_certificate == false ? 1 : 0
-
+  count   = var.enabled && var.custom_certificate == false ? 1 : 0
   name    = var.certificate_name
   type    = var.certificate_type
   domains = var.domain_names
